63d567e813f59bb3316f03d9b6d374de65e9c6b6
AgasthaProjects <33692351+AgasthaProjects@users.noreply.github.com>
2018-04-10 06:44:17 +0530

    Initial commit

---
+++ app/src/main/java/com/insurance/insuranceapp/Activites/LoginActivity.java
@@ -0,0 +1,33 @@
+package com.insurance.insuranceapp.Activites;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+import com.insurance.insuranceapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+    private Button signin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        signin = (Button)findViewById(R.id.signin);
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+}
