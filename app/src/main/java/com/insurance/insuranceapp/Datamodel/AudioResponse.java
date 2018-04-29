a7b4839ebcf6f2ee6fb4ab735c3f202f15c8e4ae
GuNaA <cviacmobility@gmail.com>
2018-04-29 10:48:45 +0530

    update

---
+++ app/src/main/java/com/insurance/insuranceapp/Datamodel/AudioResponse.java
@@ -0,0 +1,23 @@
+package com.insurance.insuranceapp.Datamodel;
+
+public class AudioResponse {
+
+    private String code;
+    private String desc;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
